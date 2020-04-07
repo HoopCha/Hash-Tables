fa74b8f3393aa92b3f379814d61bfda6b11b8e47
Chase Hooper <Hoopchaser@gmail.com>
2020-04-07 17:48:06 -0600

    cleaned up code and comments, and back to working state

--- src/hashtable.py
+++ src/hashtable.py
@@ -90,20 +90,29 @@ class HashTable:
 
         Fill this in.
         '''
+        #Create the hashed key
         hash_key = self._hash_mod(key)
-        if not self.storage[hash_key]:
-            return "The key is not found here!"
+        #Create current and previous pointers
         current = self.storage[hash_key]
-        last = self.storage[hash_key]
+        previous = None
+
+        #While in the bucket
         while current:
-            if current.key == key:
-                if current.key != last.key:
-                    last.next = current.next
-                else:
-                    self.storage[hash_key] = current.next
+            #If the current key matches the key you want to remove and the previous exists
+            if current.key == key and previous:
+                #Set the previous to the current next, therefore removing the one between them from the link
+                previous.next = current.next
                 return
-            last = current
+            #Else if the current key matches the key oyu want to remove but previous doesnt exist (aka the first item in the bucket) 
+            elif current.key == key and not previous:
+                #Set the bucket equal to the next item
+                self.storage[hash_key] = current.next
+                return
+            #If neither of those happen, set previous to current, and move current to the next one, and repeat
+            previous = current
             current = current.next
+        #If not found at all return error
+        return "Key not found"
 
 
     def retrieve(self, key):
@@ -114,14 +123,20 @@ class HashTable:
 
         Fill this in.
         '''
+        #Create the hashed key
         hash_key = self._hash_mod(key)
-        if not self.storage[hash_key]:
-            return None
+        #Create the current bucket using the hash_key
         current = self.storage[hash_key]
+
+        #While in the bucket
         while current:
+            #If the current key is equal to the key you want to retrieve
             if current.key == key:
+                #Return it
                 return current.value
+            #Otherwise keep cycling through the bucket
             current = current.next
+        #Otherwise return none
         return None
 
 
@@ -132,18 +147,23 @@ class HashTable:
 
         Fill this in.
         '''
+        #Set an old storage that is equal to the current one
         old_storage = self.storage
+        #Double the capacity of the hashtable
         self.capacity *= 2
+        #Set the new storage
         self.storage = [None] * self.capacity
 
-        for node in old_storage:
-            while node:
-                self.insert(node.key, node.value)
-                node = node.next
+        #For each item in the old storage
+        for item in old_storage:
+            #Go through it and:
+            while item:
+                #Insert each item
+                self.insert(item.key, item.value)
+                #Get next item
+                item = item.next
 
 
-
-testarr =
 
 if __name__ == "__main__":
     ht = HashTable(2)
@@ -158,6 +178,7 @@ if __name__ == "__main__":
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
     print(ht.retrieve("line_3"))
+ 
 
     # Test resizing
     old_capacity = len(ht.storage)
